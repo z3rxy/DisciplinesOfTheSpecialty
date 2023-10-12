88337e8f53e9e72a0f9583551e3958921dbf169e
z3rxy <pa.volkogonov@gmail.com>
2023-10-12 17:51:18 +0300

    Some minor improvements

--- 5Lab/5Lab2Task.py
+++ 5Lab/5Lab2Task.py
@@ -3,25 +3,21 @@ from scipy.special import genlaguerre
 from scipy.integrate import quad
 import matplotlib.pyplot as plt
 
-# Определение функции f(x) = x^7
 def f(x):
     return x**7
 
-# Вычисление коэффициентов разложения a_j
 coefficients = []
 for j in range(7):
     integrand = lambda x: f(x) * genlaguerre(j, 0)(x)
     integral, _ = quad(integrand, 0, 1)
     coefficients.append(integral)
 
-# Создание приближения суммой полиномов Лагера
 def approximation(x):
     result = 0
     for j in range(7):
         result += coefficients[j] * genlaguerre(j, 0)(x)
     return result
 
-# Создание графиков
 x = np.linspace(0, 1, 1000)
 y = f(x)
 y_approximation = approximation(x)
@@ -36,6 +32,5 @@ plt.title('Function and Best Approximation')
 plt.grid()
 plt.show()
 
-# Вывод коэффициентов разложения
 for j, aj in enumerate(coefficients):
     print(f'a_{j} = {aj}')
