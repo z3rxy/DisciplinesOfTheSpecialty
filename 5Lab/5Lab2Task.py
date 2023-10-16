b29079f808c5554598385ba0d3bd0963fbe66593
z3rxy <pa.volkogonov@gmail.com>
2023-10-16 14:12:48 +0300

    Fixed 2 task in 5 lab

--- 5Lab/5Lab2Task.py
+++ 5Lab/5Lab2Task.py
@@ -1,36 +1,39 @@
 import numpy as np
-from scipy.special import genlaguerre
+from scipy.special import eval_laguerre
 from scipy.integrate import quad
 import matplotlib.pyplot as plt
 
 def f(x):
     return x**7
 
-coefficients = []
-for j in range(7):
-    integrand = lambda x: f(x) * genlaguerre(j, 0)(x)
-    integral, _ = quad(integrand, 0, 1)
-    coefficients.append(integral)
+def laguerre_poly(i):
+    return lambda x: eval_laguerre(i, x)
 
-def approximation(x):
-    result = 0
-    for j in range(7):
-        result += coefficients[j] * genlaguerre(j, 0)(x)
-    return result
+def scalar_product(f, g):
+    return quad(lambda x: f(x) * g(x), 0, 1)[0]
+
+n = 6
+A = np.zeros((n, n))
+b = np.zeros(n)
+
+for i in range(n):
+    for k in range(n):
+        A[i, k] = scalar_product(laguerre_poly(i), laguerre_poly(k))
+    b[i] = scalar_product(f, laguerre_poly(i))
+
+coefficients = np.linalg.solve(A, b)
+print("Коэффициенты разложения:", coefficients)
+
+def approximated_laguerre(x):
+    return sum(coefficients[i] * laguerre_poly(i)(x) for i in range(n))
 
 x = np.linspace(0, 1, 1000)
 y = f(x)
-y_approximation = approximation(x)
+y_approximated = approximated_laguerre(x)
 
-plt.figure(figsize=(10, 6))
-plt.plot(x, y, label='f(x) = x^7', color='blue')
-plt.plot(x, y_approximation, label='Approximation', color='red')
+plt.figure()
+plt.plot(x, y, label='x^7', linestyle='-')
+plt.plot(x, y_approximated, label='Приближение', linestyle='--')
 plt.legend()
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Function and Best Approximation')
-plt.grid()
+plt.title('Приближение функции x^7 полиномами Лагерра')
 plt.show()
-
-for j, aj in enumerate(coefficients):
-    print(f'a_{j} = {aj}')
