8c332cd394b79b0885826f6a9262a6befbabed48
z3rxy <pa.volkogonov@gmail.com>
2023-11-12 13:02:55 +0300

    Final version of lab №7

--- 7Lab/7Lab1Task.py
+++ 7Lab/7Lab1Task.py
@@ -10,21 +10,29 @@ H = np.zeros((M, N))
 h = 200 / N
 tau = T / M
 
-z = np.linspace(0, 200, N)
+z = np.linspace(1, 200, N)
 
 E[0, :] = 0.1 * np.sin(2 * np.pi * z / 100)
 H[0, :] = 0.1 * np.sin(2 * np.pi * z / 100)
 
 for j in range(M - 1):
-    for i in range(1, N): 
+    # Обработка граничных значений
+    dE0 = -tau / h * (H[j, 0] - H[j, -1])
+    dH0 = -tau / h * (E[j, -1] - E[j, 0])
+
+    E[j + 1, 0] = E[j, 0] - dE0
+    H[j + 1, 0] = H[j, 0] - dH0
+
+    for i in range(1, N):
+        # Обновление граничных значений перед внутренним циклом
+        E[j + 1, 0] = E[j, 0] - dE0
+        H[j + 1, 0] = H[j, 0] - dH0
+
         dE = -tau / h * (H[j, i] - H[j, i - 1])
         dH = -tau / h * (E[j, i - 1] - E[j, i])
 
         E[j + 1, i] = E[j, i] - dE
         H[j + 1, i] = H[j, i] - dH
-
-        #E[j + 1, E[j + 1, :] < 0] = 0
-        #H[j + 1, H[j + 1, :] < 0] = 0
 
 
 plt.subplot(121)
