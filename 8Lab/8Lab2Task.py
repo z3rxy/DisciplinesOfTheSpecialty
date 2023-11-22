b441d1317acf6aef5c3497e7562391a2806d2384
z3rxy <pa.volkogonov@gmail.com>
2023-11-22 19:35:57 +0300

    Another version of 8 lab

--- 8Lab/8Lab2Task.py
+++ 8Lab/8Lab2Task.py
@@ -3,62 +3,30 @@ import matplotlib.pyplot as plt
 
 N = 200
 M = 1000
-T = 1.0
-L = 200.0
-h = L / N
-tau = T / M
-mu = 0.1
-nu = 0.1
-
-z = np.linspace(0, L, N)
-t = np.linspace(0, T, M)
+T = 1
 
 E = np.zeros((M, N))
 H = np.zeros((M, N))
+h = 200/N
+tau = T/M
+
+z = np.linspace(0, 200, N)
 
 E[0, :] = 0.1 * np.sin(2 * np.pi * z / 100)
 H[0, :] = 0.1 * np.sin(2 * np.pi * z / 100)
 
-for j in range(M - 1):
-    dE0 = -tau / h * (H[j, 0] - H[j, -1])
-    dH0 = -tau / h * (E[j, -1] - E[j, 0])
+for j in range(M-1):
+    for i in range(N-1):
+        # Изменения в вычислении E и H
+        E[j + 1, i] = E[j, i] - tau/h * (H[j, i + 1] - H[j, i])
+        H[j + 1, i] = H[j, i] - tau/h * (E[j, i + 1] - E[j, i])
 
-    E[j + 1, 0] = E[j, 0] - dE0
-    H[j + 1, 0] = H[j, 0] - dH0
+    # Граничное условие для последнего столбца
+    E[j + 1, N - 1] = E[j + 1, 0]
+    H[j + 1, N - 1] = H[j, N-1] - tau/(2*h) * (E[j+1, 0] - E[j+1, N-2]) - (tau/4) * (E[j+1, N-1] - 2 * E[j+1, N-2] + E[j+1, N-3]) / h**2
 
-    for i in range(1, N):
-        dE = -tau / h * (H[j, i] - H[j, i - 1])
-        dH = -tau / h * (E[j, i - 1] - E[j, i])
+# Остальной код остается неизменным
 
-        E[j + 1, i] = E[j, i] - dE
-        H[j + 1, i] = H[j, i] - dH
-
-# Модификация формулы для H_i^(1/2)
-for j in range(1, M):
-    for i in range(N):
-        H_sqrt = np.sqrt(H[j, i])
-        H_sqrt_minus_H_0 = H_sqrt - np.sqrt(H[j - 1, i])
-        E_diff = (E[j, (i + 1) % N] - E[j, i]) / h
-
-        H[j, i] = (H_sqrt_minus_H_0 / (tau / 2) - (tau / 4) * nu * E_diff) ** 2
-
-# Аналитическое решение
-def exact_solution(z, t, mu, nu):
-    return 0.1 * np.sin(2 * np.pi * z / 100) * np.cos(2 * np.pi * nu * t[:, np.newaxis] / T)
-
-exact_E = exact_solution(z, t, mu, nu)
-
-# Построение графиков
-plt.subplot(121)
-plt.plot(z, E[0, :])
-plt.plot(z, E[-1, :], color='orange', label='Numerical')
-plt.plot(z, exact_E[-1, :], linestyle='dashed', label='Exact')
-plt.title('E')
-plt.legend()
-
-plt.subplot(122)
-plt.plot(z, H[0, :] ** 0.5)
-plt.plot(z, H[-1, :] ** 0.5, color='orange', label='Numerical')
-plt.title('sqrt(H)')
+# ... (ваш код построения графиков)
 
 plt.show()
